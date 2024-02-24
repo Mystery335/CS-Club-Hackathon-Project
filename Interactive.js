1231b578a26d8ab708c30874810e766226012bb9
William-Chen07 <161098503+William-Chen07@users.noreply.github.com>
2024-02-24 16:11:42 -0600

    Update Interactive.js

--- Interactive.js
+++ Interactive.js
@@ -1,3 +1,9 @@
-const myButton = document.getElementById('myButton');
+const change = document.getElementById('change');
+myButton.addEventListener('click', () => {
+    alert('Button clicked!')});
+const begin = document.getElementById('begin');
+myButton.addEventListener('click', () => {
+    alert('Button clicked!')});
+const return = document.getElementById('return');
 myButton.addEventListener('click', () => {
     alert('Button clicked!')});
