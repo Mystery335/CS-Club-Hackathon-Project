2f2796419f14cd267e04c46ccff81c48f68876c6
William-Chen07 <161098503+William-Chen07@users.noreply.github.com>
2024-02-24 17:45:33 -0600

    Update Interactive.js

--- Interactive.js
+++ Interactive.js
@@ -6,9 +6,9 @@ option1.addEventListener('click', () => {
     alert('Button clicked!')
 });
 const option2 = document.getElementById('option2');
-let hide=false;
 option2.addEventListener('click', () => {
-    hide=true});
-if(hide==true){
-    option2.style.visibilty='hidden';
-}
+    const hide = true;
+    if (hide) {
+        myButton.style.display = 'none';
+    }
+});
