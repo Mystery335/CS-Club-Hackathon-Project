82e331034a73f0583ea64065fd8dee63036493fd
William-Chen07 <161098503+William-Chen07@users.noreply.github.com>
2024-02-24 19:12:26 -0600

    Add files via upload

--- Interactive.js
+++ Interactive.js
@@ -4,5 +4,9 @@ function hideButton(clickedButton) {
     buttons.forEach((button) => {
             button.style.display = 'none'; 
     });
+    const original = document.getElementById("original").value;
+    const option1 = document.getElementById("option1").value;
+    const option2 = document.getElementById("option2").value;
+    
 }
 
