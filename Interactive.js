48c5af15139845eaea82449dade9f0a39bb0c394
William-Chen07 <161098503+William-Chen07@users.noreply.github.com>
2024-02-24 21:50:12 -0600

    Add files via upload

--- Interactive.js
+++ Interactive.js
@@ -1,12 +1,23 @@
-
+let saved;
 function hideButton(clickedButton) {
+const original = document.getElementById("original").value;
+const option1 = document.getElementById("option1").value;
+const option2 = document.getElementById("option2").value;
     const buttons = document.querySelectorAll('.option');
     buttons.forEach((button) => {
-            button.style.display = 'none'; 
+        if(clickedButton.value == original) {
+            saved = original;
+        }
+        else if (clickedButton.value == option1) {
+            saved= option1;
+        }
+        else {
+            saved = option2;
+        }
+        button.style.display = 'none'; 
     });
-    const original = document.getElementById("original").value;
-    const option1 = document.getElementById("option1").value;
-    const option2 = document.getElementById("option2").value;
-    
+    document.getElementById("savedValue").innerHTML = saved;
 }
+
+    
 
